bc4695eb4d3453882c9c9199e5c2e2129bf3c23d
martandy <zhiiqiang.chen@gmail.com>
2024-02-29 12:41:58 +0700

    Initial Commit

---
+++ SwiftFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftFirebase
+//
+//  Created by martandy chen on 29/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
